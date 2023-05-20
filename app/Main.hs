84375fb742a8b109986421a7ee9173e54a64ab59
Frank Riccobono <F.Riccobono@gmail.com>
2023-05-20 10:57:48 -0400

    initial commit - not fully working

---
+++ app/Main.hs
@@ -0,0 +1,30 @@
+module Main (main) where
+
+-- includes.hs
+import Text.Pandoc.JSON
+import Text.Pandoc
+import Text.Pandoc.Error
+import qualified Data.Text.IO as TIO
+import qualified Data.Text as T
+import Control.Monad
+
+import Prelude hiding (readFile, getLine, writeFile)
+
+stripPandoc :: Either PandocError Pandoc -> [Block]
+stripPandoc p =
+  case p of
+    Left _ -> []
+    Right (Pandoc _ blocks) -> blocks
+
+doInclude :: Block -> IO [Block]
+doInclude cb@(CodeBlock (id, classes, namevals) contents) =
+  case lookup (T.pack "include") namevals of
+       Just f     -> do
+          c <- TIO.readFile (T.unpack f)
+          p <- runIO $ readMarkdown def c
+          return $! stripPandoc p
+       Nothing    -> return [cb]
+doInclude x = return [x]
+
+main :: IO ()
+main = toJSONFilter doInclude
